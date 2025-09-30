02a4319ea56d31e062a945485003b4397b90705e
shreeraj bhosle <shreeraj.bhosle@akasaair.com>
2025-09-30 15:40:46 +0530

    Add all new files including images

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AppNavbar from './components/Navbar';
+import Herocarousel from './components/Carousel';
 function App() {
   return (
     <>
     <AppNavbar />
+    <Herocarousel />
     </>
   );
 }
